a1538e24985f48e082b55ff73cd61980e27fe883
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-05-24 11:52:35 +0200

    updated for Exercise 4.7

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('e54680fa2228400280972b00aefca16f').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
